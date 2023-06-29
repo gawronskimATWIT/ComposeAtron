7700b7bf2cfbebfc8de3cf6acaca9fbd9e4583d5
gawronskim <gawronskim@wit.edu>
2023-06-29 12:54:04 -0400

    Added artistName syntax check and artist genre valid.

--- metadata/getSongMetaData.py
+++ metadata/getSongMetaData.py
@@ -2,6 +2,7 @@ import SpotifyAuth
 import pandas as pd
 import json
 import time
+from metdataHelpers import isRapArtist, validateArtistName
 from pymongo import MongoClient
 from random import uniform
 from retrying import retry
@@ -18,46 +19,54 @@ def make_request(spotify_func, *args, **kwargs):
     return spotify_func(*args, **kwargs)
 
 
+
 pbar = tqdm(total=df.shape[0], ncols=80)
 
 for index, row in df.iterrows():
+    
     artistName = row['artist_mb']
 
-    artistResult = make_request(spotify.search, q='artist:' + artistName, type='artist', limit = 1)
-    time.sleep(uniform(0.1, 0.3)) # delay between each request
+    artistName = validateArtistName(artistName)
 
-    if len(artistResult['artists']['items'] ) > 0:
-        artistID = artistResult['artists']['items'][0]['id']
-
-        #Get albums
-        albums = make_request(spotify.artist_albums, artistID, album_type='album')
+    if isRapArtist(artistName,spotify):
+    
+        #API Call to get artist
+        artistResult = make_request(spotify.search, q='artist:' + artistName, type='artist', limit = 1)
         time.sleep(uniform(0.1, 0.3)) # delay between each request
 
-       # Get all albums
-    for album in albums['items']:
-        albumID = album['id']
-        albumName = album['name']
+        #Check if artist exists
+        if len(artistResult['artists']['items'] ) > 0:
+            artistID = artistResult['artists']['items'][0]['id']
 
-    # Fetch the album's tracks
-        tracks = make_request(spotify.album_tracks, albumID)
-        time.sleep(uniform(0.1, 0.3)) # delay between each request
+            #Get albums
+            albums = make_request(spotify.artist_albums, artistID, album_type='album')
+            time.sleep(uniform(0.1, 0.3)) # delay between each request
 
-    # Grab more metadata from each song
-        for track in tracks['items']:
-        # Create a JSON object for the song
-            song_data = {
-            'artist_name': artistName,
-            'song_name': track['name'],
-            'song_id': track['id'],
-            'album_name': albumName,
-            'album_id': albumID,
-            'release_date': album['release_date']
-        }
+        # Get all albums
+        for album in albums['items']:
+            albumID = album['id']
+            albumName = album['name']
 
-        # Use the artist's name as the collection name
-            collection = db[artistName]
+        # Fetch the album's tracks
+            tracks = make_request(spotify.album_tracks, albumID)
+            time.sleep(uniform(0.1, 0.3)) # delay between each request
 
-        # Insert the song data into the collection
-            collection.insert_one(song_data)
-    pbar.update(1)
+        # Grab more metadata from each song
+            for track in tracks['items']:
+            # Create a JSON object for the song
+                song_data = {
+                'artist_name': artistName,
+                'song_name': track['name'],
+                'song_id': track['id'],
+                'album_name': albumName,
+                'album_id': albumID,
+                'release_date': album['release_date']
+            }
+
+            # Use the artist's name as the collection name
+                collection = db[artistName]
+
+            # Insert the song data into the collection
+                collection.insert_one(song_data)
+        pbar.update(1)
 pbar.close()
